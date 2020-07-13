c76bdeb020a9a126d5adb8c37c2f54b3e6ea9134
Jessie Effah <Jekwakyewa@gmail.com>
2020-07-13 20:55:55 +0100

    functionality to display the words per minute added

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ const testArea = document.querySelector("#test-area");
 const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
+var WORDSPERMINUTE = document.querySelector("#result-table td#wpm strong");
 
 //creating a global variable called timer set it an array of minutes, seconds , hundreth of seconds and thousandths of seconds
 var timer = [0,0,0,0];
 var interval ;
 var timerRunning = false;
+var currentTime;
+
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 function leadingZero(time){
   if(time <= 9){
@@ -16,11 +19,12 @@ function leadingZero(time){
   return time;
 }
 
-// 3. Run a standard minute/second/hundredths timer:
+// 3. Run a standard minute:second:hundredths of a second timer:
 function runTimer(){
-  let currentTime = leadingZero(timer[0])  + ":" + leadingZero(timer[1])+ ":" + leadingZero(timer[2]);
-  theTimer.innerHTML = currentTime;
-  timer[3]++; //updating the last value
+   currentTime = leadingZero(timer[0])  + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
+  // var convertTimeToMinutes = timer[0] + ;
+   theTimer.innerHTML = currentTime;
+   timer[3]++; //updating the last value
 
   timer[0] = Math.floor((timer[3]/100)/60);
   timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
@@ -28,21 +32,40 @@ function runTimer(){
 }
 
 // 4. Match the text entered with the provided text on the page:
-function spellcheck(){
-  let textEntered  = testArea.value;
-  let originTextMatch = originText.substring(0, textEntered.length);
+  function spellcheck(){
+    let textEntered  = testArea.value;
+    let originTextMatch = originText.substring(0, textEntered.length);
 
-if(textEntered == originText){
-  clearInterval(interval); //stop the timer.
-  testWrapper.style.borderColor ="#429890"; //set to green when the whole text typed matches the original text
-}else{
-  if(textEntered == originTextMatch){
-    testWrapper.style.borderColor ="#65CCf3"; //blue and typing correct letter but not done
+  if(textEntered == originText){
+    let minutesOfCurrentTime = timer[0];
+    //converting the seconds portion of the time to minutes
+    let secondsOfCurrentTimeToMinutes = (timer[1])/60;
+
+    //converting the hundredth of a second time to minutes
+    let hundrethofASecondOfCurrentTimeToMinutes = (timer[2])/6000;
+    let totalCurrentTimeInMinutes = minutesOfCurrentTime +  secondsOfCurrentTimeToMinutes + hundrethofASecondOfCurrentTimeToMinutes;
+
+    WORDSPERMINUTE.innerHTML = Math.ceil(countWords(textEntered)/totalCurrentTimeInMinutes.toFixed(4)) + " WPM";
+    clearInterval(interval); //stop the timer.
+    testWrapper.style.borderColor ="#429890"; //set to green when the whole text typed matches the original text
   }else{
-      testWrapper.style.borderColor ="#E95D0F";  //set to orange color when a wrong letter is typed
+    if(textEntered == originTextMatch){
+      testWrapper.style.borderColor ="#65CCf3"; //blue and typing correct letter but not done
+    }else{
+        testWrapper.style.borderColor ="#E95D0F";  //set to orange color when a wrong letter is typed
+      }
+   }
+}
+
+
+//method for counting the number of words
+function countWords(str) {
+           str = str.replace(/(^\s*)|(\s*$)/gi,"");
+           str = str.replace(/[ ]{2,}/gi," ");
+           str = str.replace(/\n /,"\n");
+           return str.split(' ').length;
   }
-}
-}
+
 
 // 2.Start the timer:
 function start(){
@@ -52,7 +75,7 @@ function start(){
     timerRunning = true;
   interval = setInterval(runTimer , 10); //run every 1000 of a second
   }
-  console.log(textEnteredLength);
+  //console.log(textEnteredLength);
 }
 
 
@@ -71,6 +94,9 @@ theTimer.innerHTML = "00:00:00";
 
 //set the border color around the textara back to grey
 testWrapper.style.borderColor = "grey";
+
+//clear the WORDSPERMINUTE to display "0 WPM"
+WORDSPERMINUTE.innerHTML = "0 WPM";
 }
 
 
