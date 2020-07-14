7f449ad2c7bd5ceee40f7fd97809054bca7b5cf0
Jessie Effah <Jekwakyewa@gmail.com>
2020-07-14 20:20:01 +0100

    bug fix for errors inputs done

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ var wordsPerMinute = document.querySelector("#result-table td#wpm strong");
 var numberofErrors = document.querySelector("#wrong strong");
 var errorCount = 0;
 
+
 //creating a global variable called timer set it an array of minutes, seconds , hundredth of seconds and thousandths of seconds
 var timer = [0,0,0,0];
 var interval ;
 var timerRunning = false;
-var currentTime;
 
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
@@ -26,7 +26,7 @@ function leadingZero(time){
 
 // 3. Run a standard minute:second:hundredths of a second timer:
 function runTimer(){
-   currentTime = leadingZero(timer[0])  + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
+  let currentTime = leadingZero(timer[0])  + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
   // var convertTimeToMinutes = timer[0] + ;
    theTimer.innerHTML = currentTime;
    timer[3]++; //updating the last value
@@ -36,12 +36,16 @@ function runTimer(){
   timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
 }
 
+
+
 // 4. Match the text entered with the provided text on the page:
+
   function spellcheck(){
-    let textEntered  = testArea.value;
+    var textEntered  = testArea.value;
     let originTextMatch = originText.substring(0, textEntered.length);
 
-  if(textEntered == originText){
+    if(textEntered == originText){
+    clearInterval(interval); //stop the timer.
     let minutesOfCurrentTime = timer[0];
     //converting the seconds portion of the time to minutes
     let secondsOfCurrentTimeToMinutes = (timer[1])/60;
@@ -51,18 +55,23 @@ function runTimer(){
     let totalCurrentTimeInMinutes = minutesOfCurrentTime +  secondsOfCurrentTimeToMinutes + hundrethofASecondOfCurrentTimeToMinutes;
 
     wordsPerMinute.innerHTML = Math.ceil(countWords(textEntered)/totalCurrentTimeInMinutes.toFixed(4)) + " WPM";
-    clearInterval(interval); //stop the timer.
+
     testWrapper.style.borderColor ="#429890"; //set to green when the whole text typed matches the original text
+
   }else{
     if(textEntered == originTextMatch){
       testWrapper.style.borderColor ="#65CCf3"; //blue and typing correct letter but not done
-    }else{
-        testWrapper.style.borderColor ="#E95D0F";  //set to orange color when a wrong letter is typed
-        ++errorCount;
-        numberofErrors.innerHTML = errorCount;
+      }
+
+    else{
+      testWrapper.style.borderColor ="#E95D0F";  //set to orange color when a wrong letter is typed
+      ++errorCount;
+      numberofErrors.innerHTML = errorCount;
       }
    }
+
 }
+
 
 
 //method for counting the number of words
@@ -77,18 +86,15 @@ function countWords(str) {
 // 2.Start the timer:
 function start(){
   let textEnteredLength = testArea.value.length;
-
   if(textEnteredLength === 0 && !timerRunning){
     timerRunning = true;
     interval = setInterval(runTimer , 10); //run every 1000 of a second
   }
-  //console.log(textEnteredLength);
 }
 
 
 //5.  Reset everything:
 function reset(){
-//  console.log("Reset button pressed");
 clearInterval(interval); //this ensures the browser is not running any interval in the background
 interval = null; //we do this so that we're not setting up a new interval with a new index number the next time we start the app
 timer = [0, 0, 0, 0];
@@ -106,6 +112,7 @@ testWrapper.style.borderColor = "grey";
 wordsPerMinute.innerHTML = "0 WPM";
 
 //clear the numberofErrors to display 0
+errorCount = 0;
 numberofErrors.innerHTML = "0";
 }
 
